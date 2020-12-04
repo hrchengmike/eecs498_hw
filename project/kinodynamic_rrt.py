fbe569e43ef93bab32f2a23163d62279932f2a2a
hrchengmike <16098148d@connect.polyu.hk>
2020-12-04 00:11:52 -0500

    hovercraft can compute and execute trajectory given sequence of controls, animation working

--- project/kinodynamic_rrt.py
+++ project/kinodynamic_rrt.py
@@ -27,14 +27,71 @@ def ConvertPathToTrajectory(robot,path=[]):
     planningutils.RetimeActiveDOFTrajectory(traj,robot)#,maxvelocities=ones(3),maxaccelerations=5*ones(3))
     return traj
 
-#sets the robot pose and show in gui, pose is in numpy array [x, y, theta]
-def setRobotPose(robot, pose):
-    theta = pose[2]
-    T = np.array([[np.cos(theta), -np.sin(theta), 0, pose[0]],
-                   [np.sin(theta),  np.cos(theta), 0, pose[1]],
-                   [         0,           0,       1,       0],
-                   [         0,           0,       0,       1]])
-    robot.SetTransform(T)
+class hovercraft_class:
+    def __init__(self, robot, m, I, dt, dt_vis, dt_ctl):
+        self.robot = robot
+        self.m = m #mass
+        self.I = I #moment of inertia
+        self.dt = dt #time increment during integration
+        self.dt_vis = dt_vis # time increment during visualization
+        self.dt_ctl = dt_ctl # time increment between two control input
+        self.x = np.array([-1.0, -2.0, 0, 0, 0.5, 0])
+         # state vector x = [px, py, theta, vx, vy, omega]
+
+    #execute traj which is a list of np arrays [q1, ..., qn], qi=[x, y, theta]
+    #dt is the time between adjacent qi
+    def executeTraj(self, traj):
+        for i in range(len(traj)):
+            self.setRobotPose(self.robot, traj[i])
+            time.sleep(self.dt_vis)
+
+    #sets the robot pose and show in gui, pose is in numpy array [x, y, theta]
+    def setRobotPose(self, robot, pose):
+        theta = pose[2]
+        T = np.array([[np.cos(theta), -np.sin(theta), 0, pose[0]],
+                       [np.sin(theta),  np.cos(theta), 0, pose[1]],
+                       [         0,           0,       1,       0],
+                       [         0,           0,       0,       1]])
+        self.robot.SetTransform(T)
+
+    #compute the trajectory of robot given current pose and control input
+    #returns trajectory
+    #control = list [fx, fy, t] force and torque
+    def computeTraj(self, control):
+        px = self.x[0]
+        py = self.x[1]
+        theta = self.x[2]
+        vx = self.x[3]
+        vy = self.x[4]
+        omega = self.x[5]
+        fx = control[0]
+        fy = control[1]
+        t = control[2]
+        ax = fx/self.m
+        ay = fy/self.m
+        beta = t/self.I
+        dt = self.dt
+        traj = []
+        for i in range(int(self.dt_ctl/self.dt)):
+            px = px + dt * vx
+            py = py + dt * vy
+            theta = theta + omega * dt
+            vx = vx + ax * dt
+            vy = vy + ay * dt
+            omega = omega + beta * dt
+            traj.append(np.array([px, py, theta]))
+        self.x = np.array([px, py, theta, vx, vy, omega])
+        return traj
+
+    def computeControls(self, controls):
+        traj = []
+        for control in controls:
+            self.executeTraj(self.computeTraj(control))
+
+    #compute the goal position of robot given current pose and control input
+    #returns end position
+    #def computePos(control)
+
 
 if __name__ == "__main__":
 
@@ -47,22 +104,27 @@ if __name__ == "__main__":
     env.Reset()
     # load a scene from environment XML file
     env.Load('hovercraft_env.xml')
-    time.sleep(0.1)
+    time.sleep(2.0)
 
     # 1) get the 1st robot that is inside the loaded scene
     # 2) assign it to the variable named 'robot'
     robot = env.GetRobots()[0]
     print "Robot in collision with environment?", env.CheckCollision(robot)
 
-    for i in arange(0.0, 1.0, 0.004):
-        setRobotPose(robot, np.array([i, 0, 0]))
-        time.sleep(0.01)
-    for i in arange(0.0, np.pi/2, 0.004):
-        setRobotPose(robot, np.array([1, 0, i]))
-        time.sleep(0.01)
-    for i in arange(0.0, 1.0, 0.004):
-        setRobotPose(robot, np.array([1, i, np.pi/2]))
-        time.sleep(0.01)
+    hovercraft = hovercraft_class(robot, 1, 1, 0.001, 0.001, 3)
+    traj = []
+    for i in arange(0.0, 1.0, 0.001):
+        traj.append(np.array([i, 0, 0]))
+    for i in arange(0.0, 1.0, 0.001):
+        traj.append(np.array([1, 0, i]))
+    for i in arange(0.0, 1.0, 0.001):
+        traj.append(np.array([1, i, np.pi/2]))
+    control = [1, 0, 0]
+    #traj2 = hovercraft.computeTraj(control)
+    #print traj2[-1]
+    controls = [[1,0,0], [-1,0,0],[-1,0,0],[1,0,0]]
+    hovercraft.computeControls(controls)
+    #hovercraft.executeTraj(traj2)
 
     raw_input("Press enter to exit...")
     env.Destroy()
